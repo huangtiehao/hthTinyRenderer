e048d9a50facdf02e93dd3d30c3c54cd04e9bd17
huangtiehao <1634457608@qq.com>
2023-07-07 22:15:30 +0800

    lesson5

--- TestGit/main.cpp
+++ TestGit/main.cpp
@@ -12,9 +12,9 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 const int width = 800;
 const int height = 800;
-Vec3<float>eye = { 1,1,3 };
-Vec3<float>center = { 0,0,0 };
-Vec3<float>worldUp = { 0,1,0 };
+Vec3f eye = { 1,1,3 };
+Vec3f center = { 0,0,0 };
+Vec3f worldUp = { 0,1,0 };
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)//在image上的点(x0,y0)和点(x1,y1)之间画一条color的线
 {
 	if (x0 == x1 && y0 == y1)//如果是同一个点，则只画这一个点
@@ -53,21 +53,19 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)//在im
 Matrix ModelView()
 {
 	Matrix M=Matrix::identity(4);
-	Matrix inv_t = Matrix::identity(4);
 	Vec3<float>i1;
 	Vec3<float>j1;
 	Vec3<float>k1;
-	k1 = eye - center;
-	i1 = k1 ^ worldUp;
-	j1 = i1 ^ k1;
+	k1 = (eye - center).normalize();
+	i1 = (worldUp^k1).normalize();
+	j1 = ( k1^i1).normalize();
 	for (int i = 0; i < 3; ++i)
 	{
 		M[0][i] = i1[i];
 		M[1][i] = j1[i];
 		M[2][i] = k1[i];
-		inv_t[i][3] = -eye[i];
+		M[i][3] = -center[i];
 	}
-	M = M * inv_t;
 	return M;
 }
 void rasterize(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color, int ybuffer[])
@@ -98,7 +96,7 @@ Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {//求重心坐标的u，v
 		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 	return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
-void triangle(Vec3f* t,Vec2i* uv,float **zbuffer, float intensity,TGAImage &image)
+void triangle(Vec3f* t,Vec2i* uv,float **zbuffer, float* intensity,TGAImage &image)
 {
 
 	Vec2i bboxmin;
@@ -130,18 +128,48 @@ void triangle(Vec3f* t,Vec2i* uv,float **zbuffer, float intensity,TGAImage &imag
 			{
 				uvP = uv[0] * u[0] + uv[1] * u[1] + uv[2] * u[2];
 				zbuffer[x][y] = p[2];
-				TGAColor color = model->diffuse(uvP);
-				image.set(x,y, TGAColor(color.bgra[2] * intensity, color.bgra[1] * intensity, color.bgra[0] * intensity));
+				float interpolated_intensity = intensity[0] * u[0] + intensity[1] * u[1] + intensity[2] * u[2];
+				image.set(x,y, TGAColor(255,255,255)*interpolated_intensity);
 			}
 		}
 	}
 }
-Vec3f world2screen(Vec3f v) {
-	return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+Vec3f MatrixToVec3f(Matrix m)
+{
+	Vec3f res;
+	res[0] = m[0][0];
+	res[1] = m[1][0];
+	res[2] = m[2][0];
+	return res;
+}
+Matrix Vec3fToMatrix(Vec3f p)
+{
+	Matrix m(4,4);
+	m[0][0]=p[0];
+	m[1][0]=p[1];
+	m[2][0]=p[2];
+	return m;
+}
+Vec3<float> world2screen(Vec3<float> v) {
+	return Vec3<float>(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+}
+Matrix ViewPort(int x, int y, int w, int h)
+{
+	int f = 255;
+	int n = 0;
+	Matrix res=Matrix::identity(4);
+	res[0][0] = w / 2.;
+	res[1][1] = h / 2.;
+	res[2][2] = (f - n) / .2;
+	res[0][3] = x + w / 2.;
+	res[1][3] = y + h / 2.;
+	res[2][3] = (f+n) / 2.;
+	return res;
 }
 int main(int argc, char** argv) {
 
-	Vec3f light_dir = { 0,0,-1 };
+	Vec3f light_dir = { 1,-1,1};
+	light_dir.normalize();
 	TGAImage image(width, height, TGAImage::RGB);
 	model = new Model("modelObject/african_head/african_head.obj");
 	float* zbuffer[width];
@@ -158,33 +186,30 @@ int main(int argc, char** argv) {
 	}
 
 	int m = model->nfaces();
-	//Matrix M = ModelView();
-	//Matrix Projection= Matrix::identity(4);
-
+	Matrix M = ModelView();
+	Matrix P= Matrix::identity(4);
+	P[3][2] = -1.f / (eye - center).norm();
 	for (int i = 0; i < m; ++i)
 	{
 		std::vector<int>face = model->face(i);
-		Vec3<float> worldC[3];
-		Vec3<float> screenC[3];
-
+		Vec3f worldC[3];
+		Vec3f screenC[3];
+		float* intensity = new float[3];
 		for (int j = 0; j < 3; j++)
 		{
 			worldC[j] = model->vert(face[j]);
 
-			worldC[j] = worldC[j]* (1./(1 - worldC[j].z/3));
-			screenC[j] = world2screen(worldC[j]);
+			screenC[j] = Vec3f(ViewPort(width/8, height/8, width*3/4, height*3/4) * P * M * Matrix(worldC[j]));
+			intensity[j] = model->norm(i, j) * light_dir;
 		}
-		Vec3<float> n_ = (worldC[2]-worldC[1])^ (worldC[1]-worldC[0]);
+		Vec3<float> n_ = (worldC[2] - worldC[1]) ^ (worldC[1] - worldC[0]);
 		n_.normalize();
-		float intensity = n_.x * light_dir.x + n_.y * light_dir.y + n_.z * light_dir.z;
-		if (intensity > 0)
-		{
-			Vec2i uv[3];
-			for (int k = 0; k < 3; k++) {
-				uv[k] = model->uv(i, k);
-			}
-			triangle(screenC, uv, zbuffer,intensity, image);
+
+		Vec2i uv[3];
+		for (int k = 0; k < 3; k++) {
+			uv[k] = model->uv(i, k);
 		}
+		triangle(screenC, uv, zbuffer, intensity, image);
 	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
